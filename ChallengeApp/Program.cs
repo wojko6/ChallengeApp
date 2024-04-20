7a2db148a5fd57dc3b7930f6804fae665cc82c8f
Wojciech Kołłątaj <wojciech.konstanty.kollataj@gmail.com>
2024-04-20 16:09:14 +0200

    Day 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
 ﻿
-Console.WriteLine("Hello, World!");
+
+var name = "Monika";
+var age = 25;
+var sex = "Kobieta";
+
+if (sex == "Kobieta" && age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+ else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "Mężczyzna" && age < 10)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
